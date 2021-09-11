6d5e65eb3c897f5b5f499026185cfdf39fef8f25
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-09-11 12:06:29 +0700

    ✨ Sort file by file name

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,67 @@ struct LastCommit {
     file_type: FileType,
 }
 
+struct Row {
+    file_name: String,
+    time_since: String,
+    summary: String,
+}
+
+fn duration_to_time_since(duration: u64) -> String {
+    let time_since;
+    if duration < 60 {
+        time_since = format!("{} seconds ago", duration);
+    } else if duration < 3600 {
+        time_since = format!("{} minutes ago", duration / 60);
+    } else if duration < 86400 {
+        time_since = format!("{} hours ago", duration / 3600);
+    } else if duration < 2678400 {
+        time_since = format!("{} days ago", duration / 86400);
+    } else {
+        time_since = format!("{} months ago", duration / 2678400);
+    }
+
+    return time_since;
+}
+
+fn sort_file(unorder_files: HashMap<String, LastCommit>) -> Vec<Row> {
+    let mut directory_rows: Vec<Row> = Vec::new();
+    let mut file_rows: Vec<Row> = Vec::new();
+
+    let current_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    for (path, last_commit) in unorder_files.iter() {
+        let since_last_commit = current_time - (last_commit.time as u64);
+        let time_since = duration_to_time_since(since_last_commit);
+
+        let file_name = match last_commit.file_type {
+            FileType::File => format!(" {}", path),
+            FileType::Directory => format!(" {}", path),
+        };
+
+        let row = Row {
+            file_name,
+            time_since,
+            summary: last_commit.clone().summary,
+        };
+
+        match last_commit.file_type {
+            FileType::File => file_rows.push(row),
+            FileType::Directory => directory_rows.push(row),
+        };
+    }
+
+    directory_rows.sort_by(|a, b| a.file_name.cmp(&b.file_name));
+    file_rows.sort_by(|a, b| a.file_name.cmp(&b.file_name));
+
+    directory_rows.extend(file_rows);
+
+    return directory_rows;
+}
+
 fn main() -> Result<(), Error> {
     let matches = App::new("ls-git")
         .version("1.0.0")
@@ -114,30 +175,9 @@ fn main() -> Result<(), Error> {
         }
     }
 
-    let mut time_since_last_commit;
-    for (path, last_commit) in mtimes.iter() {
-        let since_last_commit = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_secs()
-            - (last_commit.time as u64);
-
-        if since_last_commit < 60 {
-            time_since_last_commit = format!("{} seconds ago", since_last_commit);
-        } else if since_last_commit < 3600 {
-            time_since_last_commit = format!("{} minutes ago", since_last_commit / 60);
-        } else if since_last_commit < 86400 {
-            time_since_last_commit = format!("{} hours ago", since_last_commit / 3600);
-        } else if since_last_commit < 2678400 {
-            time_since_last_commit = format!("{} days ago", since_last_commit / 86400);
-        } else {
-            time_since_last_commit = format!("{} months ago", since_last_commit / 2678400);
-        }
-
-        println!(
-            "{} {} {}",
-            path, last_commit.summary, time_since_last_commit
-        );
+    let rows = sort_file(mtimes);
+    for row in rows.iter() {
+        println!("{} {} {}", row.file_name, row.summary, row.time_since);
     }
 
     Ok(())
