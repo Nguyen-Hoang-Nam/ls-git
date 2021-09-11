7315d96ccce43b27af4a2fc1ddf30a0520b662fb
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-09-11 17:31:52 +0700

    ✨ Get summary of subdirectory

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 use clap::{App, Arg};
 use git2::Repository;
 use model::{FileType, LastCommit};
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, path::PathBuf};
 use utils::{print_rows, sort_file};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -16,17 +16,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .get_matches();
 
     let directory = matches.value_of("INPUT").unwrap_or(".");
+    let directory_path = PathBuf::from(directory).canonicalize()?;
 
     let mut path_and_last_commit: HashMap<String, LastCommit> = HashMap::new();
 
     let repository = Repository::discover(directory)?;
+    let repository_path = repository.path().parent().unwrap();
+
+    let relative_path = directory_path.strip_prefix(repository_path)?;
+
     let mut revwalk = repository.revwalk()?;
 
     revwalk.set_sorting(git2::Sort::TIME)?;
     revwalk.push_head()?;
     for commit_id in revwalk {
-        let commit_id = commit_id?;
-        let commit = repository.find_commit(commit_id)?;
+        let commit = repository.find_commit(commit_id?)?;
 
         if commit.parent_count() == 1 {
             let tree = commit.tree()?;
@@ -35,54 +39,61 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             let diff = repository.diff_tree_to_tree(Some(&prev_tree), Some(&tree), None)?;
 
             for delta in diff.deltas() {
-                let file_path = delta.new_file().path().unwrap();
-                let file_path_str = file_path.to_str().unwrap();
+                let mut file_path = delta.new_file().path().unwrap();
+                if file_path.starts_with(relative_path) {
+                    file_path = file_path.strip_prefix(relative_path)?;
 
-                let file_path_components = file_path_str.split('/').collect::<Vec<&str>>();
+                    let mut file_path_str = file_path.to_str().unwrap();
 
-                let file_type;
-                let file_path_decorate;
-                if file_path_components.len() > 1 {
-                    file_path_decorate = &file_path_components[0];
-                    file_type = FileType::Directory;
-                } else {
-                    file_path_decorate = &file_path_str;
-                    file_type = FileType::File;
+                    let file_path_components = file_path_str.split('/').collect::<Vec<&str>>();
+
+                    let file_type;
+                    if file_path_components.len() > 1 {
+                        file_path_str = &file_path_components[0];
+                        file_type = FileType::Directory;
+                    } else {
+                        file_type = FileType::File;
+                    }
+
+                    // let unix_time = commit.time().seconds();
+
+                    let last_commit = LastCommit {
+                        summary: commit.summary().unwrap().to_string(),
+                        time: commit.time().seconds(),
+                        file_type,
+                    };
+
+                    path_and_last_commit
+                        .entry(file_path_str.to_string())
+                        // .and_modify(|t| {
+                        //     *t = if t.time < unix_time {
+                        //         last_commit.clone()
+                        //     } else {
+                        //         t.clone()
+                        //     }
+                        // })
+                        .or_insert(last_commit);
                 }
-
-                let file_mod_time = commit.time();
-                let summary = commit.summary();
-                let unix_time = file_mod_time.seconds();
-
-                let last_commit = LastCommit {
-                    summary: summary.unwrap().to_string(),
-                    time: unix_time,
-                    file_type,
-                };
-
-                path_and_last_commit
-                    .entry(file_path_decorate.to_string())
-                    // .and_modify(|t| {
-                    //     *t = if t.time < unix_time {
-                    //         last_commit.clone()
-                    //     } else {
-                    //         t.clone()
-                    //     }
-                    // })
-                    .or_insert(last_commit);
             }
         } else if commit.parent_count() == 0 {
-            let tree = commit.tree()?;
+            let mut tree = commit.tree()?;
+
+            if relative_path.to_str().unwrap() != "" {
+                let entry_id = tree.get_path(relative_path)?.id();
+
+                tree = repository.find_tree(entry_id)?;
+            }
 
             for entry in tree.iter() {
-                let file_mod_time = commit.time();
-                let summary = commit.summary();
-                let unix_time = file_mod_time.seconds();
+                // let unix_time = commit.time().seconds();
 
                 let last_commit = LastCommit {
-                    summary: summary.unwrap().to_string(),
-                    time: unix_time,
-                    file_type: FileType::File,
+                    summary: commit.summary().unwrap().to_string(),
+                    time: commit.time().seconds(),
+                    file_type: match entry.kind().unwrap() {
+                        git2::ObjectType::Tree => FileType::Directory,
+                        _ => FileType::File,
+                    },
                 };
 
                 path_and_last_commit
@@ -111,13 +122,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .unwrap()
             .to_string();
 
-        if name != ".git" {
-            match path_and_last_commit.get(&name) {
-                Some(value) => {
-                    current_files.insert(name.to_string(), value.to_owned());
-                }
-                None => {}
+        match path_and_last_commit.get(&name) {
+            Some(value) => {
+                current_files.insert(name.to_string(), value.to_owned());
             }
+            None => {}
         }
     }
 
